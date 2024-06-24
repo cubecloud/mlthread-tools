42208a187e1aaf44fa3f79308bb82d87f8325686
cubecloud <rainmaverick@mail.ru>
2024-06-24 11:53:04 +0300

    + updated requirements.txt and setup.py

---
+++ mlthread_tools/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mlthread-tools',
+    version='0.051',
+    packages=[''],
+    package_dir={'': 'mlthread_tools'},
+    url='https://github.com/cubecloud/mlthread-tools',
+    license='MIT',
+    author='cubecloud',
+    author_email='zenroad60@gmail.com',
+    description='tools for syncing multi threads and collaborate across threads'
+)
